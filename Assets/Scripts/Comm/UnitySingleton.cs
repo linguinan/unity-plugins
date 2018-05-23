b8fa21093e7782d1363e23d0f5232feee8b557f1
munan008 <linguinan@126.com>
2018-05-23 19:44:59 +0800

    add dir and comm scripts

---
+++ Assets/Scripts/Comm/UnitySingleton.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+public abstract class UnitySingleton<T> : MonoBehaviour where T : Component
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType(typeof(T)) as T;
+                if (_instance == null)
+                {
+                    var obj = new GameObject("[--- " + typeof(T).Name + " ---]");
+                    _instance = (T)obj.AddComponent(typeof(T));
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (_instance == null)
+        {
+            _instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PluginListener.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * @Author: lgn 
+ * @Date: 2018-05-23 19:43:55 
+ * @Last Modified by:   lgn 
+ * @Last Modified time: 2018-05-23 19:43:55 
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//接收所有底层回调的函数
+public class PluginListener : UnitySingleton<PluginListener> {
+
+
+    public void Test(string status)
+    {
+
+    }
+
+
+}
