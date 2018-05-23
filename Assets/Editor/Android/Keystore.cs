3bdf0dc4a387e92d2e0e760e0af04522a8c7fe69
munan008 <linguinan@126.com>
2018-05-23 20:14:31 +0800

    add android build settings

---
+++ Assets/Editor/Android/Keystore.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+
+[InitializeOnLoad]
+public class PreloadKeystoreSetting {
+
+	static PreloadKeystoreSetting () {
+		PlayerSettings.Android.keystorePass = "111111";
+		PlayerSettings.Android.keyaliasPass = "111111";
+	}
+	
+}
